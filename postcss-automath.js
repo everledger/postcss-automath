ef01bdb2bb24a030756e608b5c0f828b684bc7ca
pospi <pospi@spadgos.com>
2016-10-17 17:13:20 +1000

    remove resolve() function presence as a requirement for activating parsing

--- postcss-automath.js
+++ postcss-automath.js
@@ -6,12 +6,10 @@
  * @package: postcss-automath
  * @author:  pospi <sam@everledger.io>
  * @since:   2016-10-17
- * @flow
  */
 
 var postcss = require('postcss');
 var helpers = require('postcss-message-helpers');
-var reduceFunctionCall = require('reduce-function-call');
 var maths = require('mathjs');
 
 var PREFIXES = maths.type.Unit.PREFIXES;
@@ -42,33 +40,14 @@ maths.import({
     strip: strip
 });
 
-function transformResolve(value) {
-    return reduceFunctionCall(value, 'resolve', function(argString) {
+function transformMath(argString) {
+    var res = maths.eval(argString);
+    // Add previous splitted unit if any
+    var formatted = res.toString();
 
-        var unit = '';
-        if (argString.indexOf('floor(') >= 0
-                || argString.indexOf('ceil(') >= 0) {
-            // drop unit to apply floor or ceil function
-            var start = argString.indexOf('(') + 1;
-            var end = argString.indexOf(')');
-            var numberWithUnit = argString.substring(start, end);
-
-
-            var number = numberWithUnit.replace(
-                /([^a-zA-Z]+)([a-zA-Z]*)$/, '$1'
-            );
-            unit = numberWithUnit.replace(/([^a-zA-Z]+)([a-zA-Z]*)$/, '$2');
-            argString = argString.substring(0, start) + number + ')';
-        }
-
-        var res = maths.eval(argString);
-        // Add previous splitted unit if any
-        var formatted = res.toString() + unit;
-
-        // Math.JS puts a space between numbers and units, drop it.
-        formatted = formatted.replace(/(.+) ([a-zA-Z]+)$/, '$1$2');
-        return formatted;
-    });
+    // Math.JS puts a space between numbers and units, drop it.
+    formatted = formatted.replace(/(.+) ([a-zA-Z]+)$/, '$1$2');
+    return formatted;
 }
 
 module.exports = postcss.plugin('postcss-math', function () {
@@ -93,7 +72,7 @@ module.exports = postcss.plugin('postcss-math', function () {
             }
 
             node[nodeProp] = helpers.try(function () {
-                return transformResolve(node[nodeProp]);
+                return transformMath(node[nodeProp]);
             }, node.source);
         })
     };
